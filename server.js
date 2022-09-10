2d960c1671934e39f2e46c23f0b6d886e44bda16
BayanBushnaq <bayanmohdali@yahoo.com>
2022-09-10 22:39:34 +0300

    done

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.use(express.json());
 const {response}=require("express");
 
 const PORT = process.env.PORT || 3010;
-const mongo = process.env.mongodb;
+
 mongoose.connect("mongodb://BayanBushnaq:1234@ac-9ykgkyg-shard-00-00.9vebrc0.mongodb.net:27017,ac-9ykgkyg-shard-00-01.9vebrc0.mongodb.net:27017,ac-9ykgkyg-shard-00-02.9vebrc0.mongodb.net:27017/?ssl=true&replicaSet=atlas-14bpd6-shard-0&authSource=admin&retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
-console.log(process.env.mongodb);
+// console.log(process.env.mongodb);
 //Create schema
 const Bookschema = new mongoose.Schema({
   title: String,
   description: String,
-  status: String,
+  status: String
 });
 const Book = mongoose.model("Book", Bookschema);
 
@@ -31,21 +31,21 @@ async function seedData() {
     title: "The Secret ",
     description:
       " The Secret is a self-help book by Rhonda Byrne that explains how the law of attraction, which states that positive energy attracts positive things into your life, governs your thinking and actions, and how you can use the power of positive thinking to achieve anything you can imagine.",
-    status: "Life Changing ",
+    status: "Life Changing "
   });
 
   const Book2 = new Book({
     title: "LOLITA ",
     description:
       "a story structured as an autobiography written by Humbert Humbert during his time at the sanatorium and then in prison.",
-    status: "Favorite Five",
+    status: "Favorite Five"
   });
 
   const Book3 = new Book({
     title: "Rich Dad Poor Dad ",
     description:
       "tells the story of a boy with two fathers, one rich, one poor, to help you develop the mindset and financial ",
-    status: "Recommended To Me",
+    status: "Recommended To Me"
   });
   await Book1.save();
   await Book2.save();
@@ -79,7 +79,7 @@ async function addBooksHandler(req, res) {
   await Book.create({
     title: req.body.title,
     description: req.body.description,
-    status: req.body.status,
+    status: req.body.status
 
   });
   Book.find({},(err,result)=>{
